df59e556ff026b0d5d7c895d91dde8dda6136e53
cauaa <cauaamorim05@gmail.com>
2023-03-15 14:33:36 -0300

    First commit

---
+++ src/com/dio/PrimerioPrograma.java
@@ -0,0 +1,20 @@
+package com.dio;
+
+import com.dio.model.Gato;
+
+public class PrimerioPrograma {
+
+	public static void main(String[] args) {
+		
+		
+		Gato gato = new Gato();
+		
+	}
+
+	
+	class livros{
+		private String nome;
+		private String npag;
+		
+	}
+}
